3fb2f52ee22cddc4a7c7daead9de7c9a3fb34715
sh-song <61382454+sh-song@users.noreply.github.com>
2022-09-11 21:04:03 +0900

    Update run_velodyne.sh

--- workspace/run_velodyne.sh
+++ workspace/run_velodyne.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf catkin_ws/src/build/ catkin_ws/src/devel
+rm -rf /workspace/catkin_ws/src/build/ /workspace/catkin_ws/src/devel
 cd catkin_ws && catkin_make
 source /workspace/catkin_ws/devel/setup.bash
 roslaunch velodyne_pointcloud VLP16_points.launch
